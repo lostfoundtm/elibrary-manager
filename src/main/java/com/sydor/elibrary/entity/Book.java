cfd09c22f817bff362e8bfa3d647574361ddcb77
Panas Sydor <panas_sydor@epam.com>
2016-11-25 16:14:42 +0200

    added book entity and repository

---
+++ src/main/java/com/sydor/elibrary/entity/Book.java
@@ -0,0 +1,64 @@
+package com.sydor.elibrary.entity;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "seq", allocationSize = 1000)
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    private Long id;
+    private String name;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (name != null ? !name.equals(book.name) : book.name != null) return false;
+        return author != null ? author.equals(book.author) : book.author == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return author + " \"" + name + "\"";
+    }
+}
