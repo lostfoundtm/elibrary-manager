25580e4c780ba9c420ec2ff046a40b43bf286094
Panas Sydor <panas_sydor@epam.com>
2016-11-25 16:15:30 +0200

    added book service with impl

---
+++ src/main/java/com/sydor/elibrary/service/BookService.java
@@ -0,0 +1,16 @@
+package com.sydor.elibrary.service;
+
+import com.sydor.elibrary.entity.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    Book save(Book book);
+
+    void remove(Long id);
+
+    Book find(Book book);
+
+    List<Book> findAll();
+}
---
+++ src/main/java/com/sydor/elibrary/service/impl/BookServiceImpl.java
@@ -0,0 +1,37 @@
+package com.sydor.elibrary.service.impl;
+
+import com.sydor.elibrary.entity.Book;
+import com.sydor.elibrary.repository.BookRepository;
+import com.sydor.elibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void remove(Long id) {
+        bookRepository.delete(id);
+    }
+
+    @Override
+    public Book find(Book book) {
+        return bookRepository.findOne(Example.of(book));
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
